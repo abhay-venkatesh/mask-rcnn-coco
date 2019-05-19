f89cb9607a54dbc013c1271cfb830190e51c2acf
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2019-05-19 12:00:12 -0500

    stop

--- lib/agents/coco_stuff_evaluator.py
+++ lib/agents/coco_stuff_evaluator.py
@@ -4,3 +4,14 @@ from lib.agents.agent import Agent
 class COCOStuffEvaluator(Agent):
     def run(self):
         raise NotImplementedError
+        dataset_val = CocoDataset()
+        val_type = "val" if args.year in '2017' else "minival"
+        coco = dataset_val.load_coco(
+            args.dataset,
+            val_type,
+            year=args.year,
+            return_coco=True,
+            auto_download=args.download)
+        dataset_val.prepare()
+        print("Running COCO evaluation on {} images.".format(args.limit))
+        evaluate_coco(model, dataset_val, coco, "bbox", limit=int(args.limit)
