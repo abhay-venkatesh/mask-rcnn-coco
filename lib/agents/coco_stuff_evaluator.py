72bcbc50b7e6a1b99a8c9f5341593ee4769a2a90
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2019-05-19 12:06:18 -0500

    stop for now

--- lib/agents/coco_stuff_evaluator.py
+++ lib/agents/coco_stuff_evaluator.py
@@ -1,17 +1,22 @@
+from lib.datasets.coco import CocoDataset
 from lib.agents.agent import Agent
+from lib.mask_rcnn.model import MaskRCNN
 
 
 class COCOStuffEvaluator(Agent):
+    YEAR = 2017
+    LEN_DATASET = 21000
+
     def run(self):
-        raise NotImplementedError
         dataset_val = CocoDataset()
-        val_type = "val" if args.year in '2017' else "minival"
+        val_type = "val" if self.YEAR in '2017' else "minival"
         coco = dataset_val.load_coco(
-            args.dataset,
+            self.config["dataset path"],
             val_type,
-            year=args.year,
+            year=self.YEAR,
             return_coco=True,
-            auto_download=args.download)
+            auto_download=False)
         dataset_val.prepare()
-        print("Running COCO evaluation on {} images.".format(args.limit))
-        evaluate_coco(model, dataset_val, coco, "bbox", limit=int(args.limit)
+        print("Running COCO evaluation on {} images.".format(self.LEN_DATASET))
+        net = MaskRCNN(mode="inference", config=config, model_dir=args.logs)
+        evaluate_coco(net, dataset_val, coco, "bbox", limit=self.LEN_DATASET)
