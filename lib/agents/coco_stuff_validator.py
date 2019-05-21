77e8e23ab3b2f0a18c9d9eca440bac20bb1f95ec
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2019-05-21 08:35:43 -0500

    stop for now

--- lib/agents/coco_stuff_validator.py
+++ lib/agents/coco_stuff_validator.py
@@ -1,15 +1,25 @@
-from lib.datasets.coco import CocoDataset
 from lib.agents.agent import Agent
+from lib.datasets.coco import CocoDataset
+from lib.mrcnn import coco_eval
 from lib.mrcnn.model import MaskRCNN
+import numpy as np
+import time
+
+
+class InferenceConfig(coco_eval.CocoConfig):
+    # Set batch size to 1 since we'll be running inference on
+    # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
 
 
 class COCOStuffValidator(Agent):
     YEAR = 2017
-    LEN_DATASET = 21000
 
     def run(self):
         dataset_val = CocoDataset()
         val_type = "val"
+        inference_config = InferenceConfig()
         coco = dataset_val.load_coco(
             self.config["dataset path"],
             val_type,
@@ -17,6 +27,49 @@ class COCOStuffValidator(Agent):
             return_coco=True,
             auto_download=False)
         dataset_val.prepare()
-        print("Running COCO evaluation on {} images.".format(self.LEN_DATASET))
-        net = MaskRCNN(mode="inference", config=config, model_dir=args.logs)
-        evaluate_coco(net, dataset_val, coco, "bbox", limit=self.LEN_DATASET)
+        net = MaskRCNN(
+            mode="inference",
+            config=inference_config,
+            model_dir=self.config["checkpoints folder"])
+        self._validate(net, dataset_val, coco)
+
+    def _validate(self, model, dataset, coco, limit=0, image_ids=None):
+        # Pick COCO images from the dataset
+        image_ids = image_ids or dataset.image_ids
+
+        # Limit to a subset
+        if limit:
+            image_ids = image_ids[:limit]
+
+        # Get corresponding COCO image IDs.
+        coco_image_ids = [dataset.image_info[id]["id"] for id in image_ids]
+
+        t_prediction = 0
+        t_start = time.time()
+
+        results = []
+        for i, image_id in enumerate(image_ids):
+            # Load images
+            image = dataset.load_image(image_id)
+
+            # Run detection
+            t = time.time()
+            r = model.detect([image], verbose=0)[0]
+            t_prediction += (time.time() - t)
+
+            # Convert results to COCO format
+            # Cast masks to uint8 because COCO tools errors out on bool
+            image_results = coco_eval.build_coco_results(
+                dataset, coco_image_ids[i:i + 1], r["rois"], r["class_ids"],
+                r["scores"], r["masks"].astype(np.uint8))
+            results.extend(image_results)
+
+        # Load results. This modifies results with additional attributes.
+        coco_results = coco.loadRes(results)
+
+        print("Prediction time: {}. Average {}/image".format(
+            t_prediction, t_prediction / len(image_ids)))
+
+        print("Total time: ", time.time() - t_start)
+
+        return coco_results
